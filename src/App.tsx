0f9abeb64d71a1b53f158b6b733343a55ec10f48
Mohammed Yassar <yaserkt786@gmail.com>
2024-01-30 11:40:50 +0000

    unUsed imports removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+
 import "./App.css";
 import ToDoList from "./components/Todolist/Todolist";
 
--- src/components/ExportButton/ExportButton.tsx
+++ src/components/ExportButton/ExportButton.tsx
@@ -1,6 +1,5 @@
 import { faFileExport } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
 import SmallButton from "../SmallButton/SmallButton";
 import Tooltip from "../Tooltip/Tooltip";
 
